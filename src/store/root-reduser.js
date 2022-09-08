5e50988c26e8a46126872e4bfb51c11a1e02dd0d
Createawish <bezlyudovalera1996@gmail.com>
2022-09-09 00:54:37 +0300

    Refactor: refactor Search component

--- src/store/root-reduser.js
+++ src/store/root-reduser.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {themeReducer} from "./theme/theme-reduser";
 import {countriesReduser} from "./countries/countries-reduser";
+import {controlsReducer} from "./controls/controls-reduser";
 
 export const rootReducer = combineReducers({
     theme: themeReducer,
     countries: countriesReduser,
+    controls: controlsReducer,
 });
