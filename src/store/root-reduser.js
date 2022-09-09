f366115aad7ed83df93809966717be6fa1250477
Createawish <bezlyudovalera1996@gmail.com>
2022-09-09 10:42:11 +0300

    Feature: create Details page, get data

--- src/store/root-reduser.js
+++ src/store/root-reduser.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import {themeReducer} from "./theme/theme-reduser";
 import {countriesReduser} from "./countries/countries-reduser";
 import {controlsReducer} from "./controls/controls-reduser";
+import {detailsReduser} from "./detalis/details-reduser";
 
 export const rootReducer = combineReducers({
     theme: themeReducer,
     countries: countriesReduser,
     controls: controlsReducer,
+    details: detailsReduser,
 });
